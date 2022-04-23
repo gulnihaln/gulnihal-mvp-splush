a4f68eb45db437f48c091dccec8d9fdbd598f372
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-04-23 18:45:00 +0100

    fix netlify error

--- src/components/Header/TopNav.js
+++ src/components/Header/TopNav.js
@@ -5,12 +5,12 @@ export default function TopNav() {
 	return (
 		<>
 			<div className="topnav">
-				<a href="#">
+				<a href="">
 					<ImTruck />
 					Make shopping easy with our Delivery Pass
 				</a>
 				<div className="vl"></div>
-				<a href="#"><GiShoppingBag />Pay in 4 with Clearpay</a>
+				<a href=""><GiShoppingBag />Pay in 4 with Clearpay</a>
 			</div>
 		</>
 	);
